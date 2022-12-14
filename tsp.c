cacd70413c51b7e7bc3c685646b51e72d6f5a1fa
Jeff Luong <56270844+JeffL2019@users.noreply.github.com>
2022-12-13 19:48:14 -0600

    Greedy Algorithm

--- tsp.c
+++ tsp.c
@@ -10,29 +10,40 @@
 #include <string.h>
 #include <omp.h>
 #include <limits.h>
+#include <time.h>
 
 // Number of cities
 #define N 1000
 
 // Matrix to store the distances between the cities
-int distances[N][N] = {0};
+int distances[N][N] = {{0}};
+int global_visited_cities[N + 1] = {0};
 int global_mincost = 99999999;
 // Function to find the minimum distance between the current city
 // and the remaining cities
 int minDistance(int currCity, int visited[N])
 {
+    // Store the minimum distance and the index of the next city
     int min = INT_MAX;
-    int minIndex;
+    int minIndex = 0;
 
+    // Loop through all cities
     for (int i = 0; i < N; i++)
     {
-        if (visited[i] == 0 && distances[currCity][i] < min)
+        // Check if the city has not been visited and is not the starting city
+        if (visited[i] == 0 && i != 0)
         {
-            min = distances[currCity][i];
-            minIndex = i;
+            // Check if the distance is less than the minimum
+            if (distances[currCity][i] < min)
+            {
+                // Update the minimum distance and the index of the next city
+                min = distances[currCity][i];
+                minIndex = i;
+            }
         }
     }
 
+    // Return the index of the next city
     return minIndex;
 }
 
@@ -41,10 +52,23 @@ int findMinCost(int visited[N])
 {
     int local_minCost = 0;
     int currCity = 0;
+    int local_visited_cities[N];
 
     for (int i = 0; i < N; i++)
     {
+        visited[i] = 0;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        local_visited_cities[i] = 0;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        // printf("i = %d, city = %d\n", i, currCity);
         visited[currCity] = 1;
+        local_visited_cities[i] = currCity;
 
         // Find the next closest city
         int nextCity = minDistance(currCity, visited);
@@ -59,10 +83,23 @@ int findMinCost(int visited[N])
     // Add the distance from the last city to the starting city
     local_minCost += distances[currCity][0];
 
+    // Set the visited flah for the starting city to 1
+    visited[0] = 1;
+
+    // Find the next closest cty (which should be the starting city)
+    int nextCity = minDistance(currCity, visited);
+
+    // Add the distance from the last city to the starting city
+    local_minCost += distances[currCity][nextCity];
+
+    // Move to the starting city
+    currCity = nextCity;
+
 #pragma omp critical
     {
         if (local_minCost < global_mincost) {
             global_mincost = local_minCost;
+            memcpy(global_visited_cities, local_visited_cities, sizeof(local_visited_cities));
         }
     }
 
@@ -75,6 +112,8 @@ int main(int argc, char *argv[])
     char buffer[8192];
     char *record, *line;
     int i = 0, j = 0;
+
+    clock_t start = clock(); // Start the time to time reading the file and the computation
 
     FILE *file;
     // Read file
@@ -93,13 +132,9 @@ int main(int argc, char *argv[])
         record = strtok(line, ",");
         while (record != NULL)
         {
-            // Check if record is a valid token
-            if (record != "")
-            {
-                // Place record in array
-                distances[i][j] = atoi(record);
-                j++;
-            }
+            // Place record in array
+            distances[i][j] = atoi(record);
+            j++;
             record = strtok(NULL, ",");
         }
         i++;
@@ -124,13 +159,32 @@ int main(int argc, char *argv[])
     // }
     // printf("\n");
 
-    // Array to keep track of which cities have been visited
-    int visited[N] = {0};
+    while ((clock() - start) / CLOCKS_PER_SEC < 60)
+    {
 
-    #pragma omp parallel num_threads(thread_count)
-    global_mincost = findMinCost(visited);
+        // Array to keep track of which cities have been visited
+        int visited[N] = {0};
+
+#pragma omp parallel num_threads(thread_count)
+        global_mincost = findMinCost(visited);
+
+    }
 
     printf("Minimum cost: %d\n", global_mincost);
+
+    printf("The number of cities traversed: %d\n", N);
+
+    printf("The list of cities reversed in order: ");
+    for (i = 0; i < N + 1; i++){
+        if (i == 1000) {
+            printf("%d", global_visited_cities[i]);
+        }
+        else {
+            printf("%d->", global_visited_cities[i]);
+        }
+    }
+
+    printf("\n");
 
     return 0;
 }
